0eaff3584e2240052504d8c2c60edcc86ae68ea2
ai-jong <ai.jon.blaine@gmail.com>
2024-09-29 08:52:14 -0700

    Add files via upload

--- MyCloud.xcframework/ios-arm64/MyCloud.framework/Headers/MyCloud.h
+++ MyCloud.xcframework/ios-arm64/MyCloud.framework/Headers/MyCloud.h
@@ -17,7 +17,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void) write:(nullable NSString*)folder merge:(BOOL)m data:(nullable NSDictionary*)item completed:(void(^)(NSDictionary *dx))ds;
 - (void) read:(nullable NSString*)folder completed:(void(^)(NSDictionary *dx))ds;
-- (void) watch:(nullable NSString*)folder completed:(void(^)(NSDictionary *dx))ds;
+- (void) monitor:(nullable NSString*)folder completed:(void(^)(NSDictionary *dx))ds;
 
 
 @end
